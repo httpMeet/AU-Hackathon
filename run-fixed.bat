59aba76bb387a5205816fab77173452271f5a8f8
httpMeet <gandhimeet81104@gmail.com>
2025-03-21 18:20:41 +0530

    Deleted Unwanted Things

--- run-fixed.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo BudgetWise Identity - JavaScript Version (Fixed)
-echo.
-echo Fixing JSX errors...
-node solve-jsx-error.js
-echo.
-echo Starting the development server...
-echo.
-npm run dev 
